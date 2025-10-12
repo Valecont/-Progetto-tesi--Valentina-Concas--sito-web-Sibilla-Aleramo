4681d608b99a07b9450db14fc866d4b945134856
Valecont <v.concas3@studenti.unipi.it>
2025-10-12 23:25:09 +0200

    sito tesi

---
+++ script.js
@@ -0,0 +1,107 @@
+let sezioni = {
+  Scrittura: {
+    titolo: "Scrittura e Emancipazione",
+    immagine: "img/penna.png",
+    testi: [
+     "La scrittura femminile occupa un posto centrale nelle riflessioni di Aleramo. Nella prefazione di Orsa Minore (1937) sottolinea l’autonomia dello spirito femminile:&quot;la scrittrice e la poetessa devono essere sé stesse, esprimere la propria realtà e il proprio mistero, lontano dalle suggestioni maschili&quot;. La fedeltà a questo principio è un filo conduttore in tutte le sue opere.",
+      "In Una donna Aleramo critica la letteratura femminile contemporanea, osservando come molte &quot;intellettuali&quot; non comprendessero che la donna può incidere solo attraverso opere che portino la propria impronta personale. Questo tema ritorna in Andando e stando e nel saggio Apologia dello spirito femminile (1911), dove denuncia una produzione “di derivazione”, imitativa e priva di originalità.",
+      "Secondo Aleramo, molte donne erano inconsapevoli della forte influenza maschile che regnava su di loro, perché la percezione del mondo era filtrata dallo sguardo maschile: &quot;su tutte le cose l’uomo ha riflettuto, poi le ha riplasmate e lanciate nella vita&quot;, mentre la donna si accontentava di questa rappresentazione. Solo una riflessione interiore e una coscienza della propria differenza permettono alla scrittura femminile di diventare autentica.",
+      "Per tutta la vita, Aleramo collega produzione letteraria ed emancipazione femminile. Critica le figure femminili idealizzate nella letteratura, lontane dalla donna reale, e mostra come il divario tra rappresentazione e realtà crei separazione tra uomini e donne. Scrivere secondo la propria esperienza permette alle donne di conoscersi meglio e di rendere pubblici gli aspetti più autentici della loro vita."
+    ]
+  },
+  Chiesa: {
+    titolo: "Il controllo religioso sul femminismo",
+    immagine: "img/chiesa.png",
+    testi: [
+      "L’educazione laica di Aleramo le permise di riconoscere quanto la Chiesa cattolica avesse ostacolato lo sviluppo del femminismo in Italia. In Una donna osserva come la religione avesse imposto per secoli il sacrificio come virtù femminile, concedendo libertà solo sotto rigida sorveglianza.",
+      "Aleramo denuncia la mancanza di una solidarietà femminile laica e l’ascesa di gruppi femministi cattolici, che rivendicavano i diritti delle donne senza mettere in discussione i dogmi cristiani. Come scrive in Appunti sulla psicologia femminile italiana (1910), l’influenza della Chiesa rimaneva forte, soprattutto nel Sud e negli ex territori papali, dove ogni iniziativa femminile era soffocata.",
+      "L’autrice sottolinea come anche le questioni politiche, come il divorzio, fossero dominate dall’autorità ecclesiastica. Questo legame tra donna e religione costituiva, per lei, uno dei principali limiti del femminismo italiano, ancora troppo condizionato dal potere clericale.Si interroga sulla conciliazione tra fede e autonomia personale.",
+      "Nel Diario di una donna (1946) Aleramo nota come, alla prima votazione femminile, molte donne fossero ancora sotto l’influenza del parroco. Questo conferma quanto la religione continuasse a condizionare la coscienza femminile. Per Aleramo, la vera emancipazione poteva avvenire solo attraverso la liberazione da tale controllo."
+    ]
+  },
+  Diritti: {
+    titolo: "Il riconoscimento dei diritti",
+    immagine: "img/femminismo.png",
+    testi: [
+     "In Appunti sulla psicologia femminile italiana (1910), Aleramo osserva come le differenze culturali tra le regioni italiane abbiano profondamente influenzato il comportamento delle donne e la diffusione delle idee di emancipazione. Queste erano presenti soprattutto nel Nord e nel Centro, mentre nel Sud restavano quasi assenti, &quot;salvo forse in qualche grande città, Napoli, Palermo, Bari, ma come spauracchio grottesco&quot;. Tale disparità derivava, secondo la scrittrice, dalle diverse dominazioni straniere: i regimi del Nord favorivano il progresso intellettuale, mentre quelli borbonici e papali avevano soffocato &quot;ogni energia morale e civile&quot;.La sua esperienza diventa testimonianza di lotta per i diritti civili e sociali delle donne.",
+      "Per Aleramo, educazione e sviluppo economico sono condizioni fondamentali per l’emancipazione femminile. Tuttavia, anche dove la donna iniziava a lavorare fuori casa, la società restava rigidamente patriarcale, ancorata all’autorità paterna e all’idea della donna come proprietà dell’uomo.",
+     "Aleramo denuncia come spesso fossero le stesse donne a non ribellarsi, sostenendo la mentalità che le opprimeva. Così contribuivano a perpetuare &quot;il barbarico concetto dell’onore e della feroce gelosia&quot;, che giustificava persino la violenza maschile in nome del possesso. Un atteggiamento diffuso in ogni ceto sociale, anche nelle campagne del Centro Italia.",
+     "L’autrice si sofferma infine su &quot;l'autorizzazione maritale&quot;, legge che vietava alle donne sposate di gestire i propri beni senza la firma del marito, (la stessa Aleramo, dopo aver lasciato la casa coniugale, Aleramo perse la custodia del figlio e i propri diritti legali) denunciando una normativa destinata a durare in un Parlamento composto solo da uomini. Per lei, l’arretratezza dei diritti femminili era frutto non solo di strutture politiche e culturali, ma anche della complicità delle donne stesse nel mantenere un sistema che limitava la loro libertà. "
+  
+    ]
+  },
+  Politica: {
+    titolo: "L’impegno politico e sociale",
+    immagine: "img/politica.png",
+    testi: [
+      "L’interesse di Sibilla Aleramo per la politica attraversa tutta la sua vita. Già in Una donna si avverte la sua attenzione verso il tema, che si approfondirà nelle opere successive. Nel capitolo In salotto (1899), pubblicato su L’Italia femminile, risponde a una lettrice sostenendo che le donne non possono restare estranee alla vita politica, perché riguarda direttamente libertà e giustizia. La politica, afferma, non è una moda né un passatempo, ma “il perno meraviglioso su cui si aggira tutta la storia dell’umanità, l’asse morale del mondo”.",
+     " Aleramo non comprende come una donna &quot;che ha un po’ di cervello e di cuore&quot; possa restare indifferente davanti alle questioni del proprio paese. La scrittrice invita le donne a riconoscersi come parte attiva della società e a superare la convinzione che la politica sia un ambito esclusivamente maschile.",
+     "Negli anni Quaranta il suo impegno politico si intensifica. Il 3 gennaio 1946 aderisce al Partito Comunista, una scelta che divide l’opinione pubblica ma che le permette di partecipare concretamente alla vita politica, collaborando anche con l’Unità. In quegli anni molti la incoraggiano a rivolgersi alle donne, riconoscendo nella sua voce una forza capace di coinvolgere e convincere.",
+      "Il  contributo di Aleramo è fondamentale perché sprona le donne a superare i ruoli tradizionali e a interessarsi ai temi civili e sociali, considerati allora &quot;pericolosi per la serenità &quot; della mente femminile. Con il suo esempio, dimostra che le capacità intellettive delle donne non sono fragili, ma pienamente in grado di comprendere, discutere e contribuire alla vita politica del paese."
+    ]
+  },
+  Femminismo: {
+    titolo: "Il femminismo come rinascita collettiva",
+    immagine: "img/diritti.png",
+    testi: [
+      "In Una donna emerge chiaramente la posizione di Aleramo sulla questione femminile. Il romanzo può essere letto come un manifesto di emancipazione, dove l’esperienza individuale diventa collettiva. Già nell’agosto 1898, con l’articolo inedito Utopia femminile, risponde alle paure maschili sull’indipendenza delle donne, chiarendo che il desiderio di maternità non verrebbe mai abbandonata le donne, in quanto istinto naturale. Inoltre, spiega che Il vero obiettivo della &quot;grande sommossa femminile&quot; è conquistare dignità, diritti politici, parità legislativa, uguaglianza nell’istruzione, libertà economica e piena individualità.",
+       " Con l’articolo Il femminismo in Italia, denuncia il ritardo del movimento italiano rispetto al resto d’Europa. Secondo Aleramo, la causa non è solo la resistenza maschile, ma anche l’accettazione passiva del ruolo tradizionale da parte delle donne aristocratiche e borghesi. Diversa è la condizione delle donne operaie, più risolute e animate da aspirazioni intellettuali, che Aleramo vede come le prime &quot;lavoratrici dell’avvenire&quot;. Tuttavia, miseria e ingiustizie sociali ne ostacolano lo sviluppo.",
+      "Negli scritti successivi, Aleramo riflette sul femminismo con toni diversi. In Andando e stando lo definisce &quot;una breve avventura, eroica all’inizio, grottesca sul finire&quot;.",
+      "Per Aleramo, la donna deve riscoprire la propria specifica individualità e dignità. Nonostante i cambiamenti nel suo giudizio sul femminismo, rimane una voce centrale per l’emancipazione femminile."
+  
+    ]
+  }
+};
+
+
+
+
+
+function mostraSezione(nomeSezione, box) {
+  var dati = sezioni[nomeSezione];
+  box.querySelector("h4").innerHTML = dati.titolo;
+  box.querySelector("img").src = dati.immagine;
+
+  var paragrafi = box.querySelectorAll(".paragrafo");
+  for (var i = 0; i < paragrafi.length; i++) {
+    paragrafi[i].innerHTML = dati.testi[i];
+  }
+
+  box.style.display = "block";
+}
+
+
+function chiudiBox(event) {
+    var contenitore = event.target.parentNode;
+    contenitore.style.display ="none"
+}
+
+ function gestoreLoad() {
+var boxS = document.getElementById("boxS");
+var boxC = document.getElementById("boxC");
+var boxP = document.getElementById("boxP");
+var boxD = document.getElementById("boxD");
+var boxF = document.getElementById("boxF");
+
+var bottoneS = document.getElementById("bottoneS");
+var bottoneC = document.getElementById("bottoneC");
+var bottoneP = document.getElementById("bottoneP");
+var bottoneD = document.getElementById("bottoneD");
+var bottoneF = document.getElementById("bottoneF");
+
+ 
+
+
+bottoneS.onclick = function() { mostraSezione("Scrittura", boxS); };
+bottoneC.onclick = function() { mostraSezione("Chiesa", boxC); };
+bottoneP.onclick = function() { mostraSezione("Diritti", boxP); };
+bottoneD.onclick = function() { mostraSezione("Politica", boxD); };
+bottoneF.onclick = function() { mostraSezione("Femminismo", boxF); };
+
+var chiudi = document.getElementsByClassName("chiudi");
+  for (var i = 0; i < chiudi.length; i++) {
+    chiudi[i].onclick = chiudiBox;
+  }
+}
+
+ window.onload = gestoreLoad;
